3f169e4081574509a712a4770944efa881ba146b
zhangjiang <896884553@q.com>
2017-09-21 08:35:52 +0800

    修改了网络请求

--- SwiftBaseProject/SwiftTools/ZJAFRequestTool.swift
+++ SwiftBaseProject/SwiftTools/ZJAFRequestTool.swift
@@ -34,7 +34,6 @@ extension ZJAFRequestTool {
     ///   - success: 成功回调
     ///   - failture: 失败回调
     class func getRequest(urlString: String, params : Parameters?, success : @escaping (_ response : AnyObject)->(), failture : @escaping (_ error : String)->()) {
-        
         let url = try! URLHandler(name: urlString).asURL()
         //使用Alamofire进行网络请求时，调用该方法的参数都是通过getRequest(urlString， params, success :, failture :）传入的，而success传入的其实是一个接受           [String : AnyObject]类型 返回void类型的函数
         ZJAFRequestTool.shareInstance.session.request(url, method: .get, parameters: params).responseJSON { (response) in
@@ -60,9 +59,7 @@ extension ZJAFRequestTool {
     ///   - success: 成功回调
     ///   - failture: 失败回调
     class func postRequest(urlString : String, params : Parameters?, success : @escaping (_ response : AnyObject)->(), failture : @escaping (_ error : String)->()) {
-        
         let url = try! URLHandler(name: urlString).asURL()
-        
         ZJAFRequestTool.shareInstance.session.request(url, method: HTTPMethod.post, parameters: params).responseJSON { (response) in
             switch response.result{
             case .success:
@@ -86,9 +83,7 @@ extension ZJAFRequestTool {
     ///   - success:
     ///   - failture:
     func upLoadImageRequest(urlString : String, params:[String:String], data: [Data], name: [String],success : @escaping (_ response : [String : AnyObject])->(), failture : @escaping (_ error : String)->()){
-        
         let headers = ["content-type":"multipart/form-data"]
-        
         ZJAFRequestTool.shareInstance.session.upload(
             multipartFormData: { multipartFormData in
                 //666多张图片上传
